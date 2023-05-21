4410ac65c65e0df46e6e020f87cb1717e6d20f5c
Tazarem <134208252+kyulman@users.noreply.github.com>
2023-05-21 21:30:16 +0900

    make homepage

---
+++ public/js/slider.js
@@ -0,0 +1,46 @@
+class Slider{
+    picture_list=[]
+    active_index=0
+    constructor(){
+        this.picture_list = $('.picture')
+    }
+    next(){
+        if(this.active_index+1>this.picture_list.length-1){
+            // active_index=this.picture_list.length-1 //혹은 0으로 되돌아가기
+            this.active_index=0
+        }else{
+            this.active_index++
+        }
+        $('.picture').removeClass('active')
+        $(this.picture_list[this.active_index]).addClass('active')
+    }
+    prev(){
+        if(this.active_index-1<0){
+            this.active_index=this.picture_list.length-1
+        }else{
+            this.active_index = this.active_index-1
+        }
+        $('.picture').removeClass('active')
+        $(this.picture_list[this.active_index]).addClass('active')
+    }
+}
+
+$(function(){
+    let slider = new Slider()
+    $('.arrow.left').click(function(){
+        slider.prev()
+    })
+
+    $('.arrow.right').click(function(){
+        slider.next()
+    })
+
+    $('.slider').on('mouseover',function(){
+        $('.arrow').addClass('active')
+    })
+    
+    $('.slider').on('mouseleave',function(){
+        $('.arrow').removeClass('active')
+    })
+})
+
